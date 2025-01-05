8ae67c756c351490202f82b0fbac7212a8769397
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-05 13:34:17 +0100

    [~] Task: Strings Mix

---
+++ CodeWars4kyu/StringsMixTask.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars4kyu
+{
+    public class StringsMixTask
+    {
+        public static string Mix(string s1, string s2)
+        {
+            var s1Letters = AddLetters(s1.ToCharArray());
+            var s2Letters = AddLetters(s2.ToCharArray());
+
+            var results = new List<string>();
+
+            var allKeys = s1Letters.Keys.Union(s2Letters.Keys);
+
+            foreach (var key in allKeys)
+            {
+                int countS1 = s1Letters.ContainsKey(key) ? s1Letters[key] : 0;
+                int countS2 = s2Letters.ContainsKey(key) ? s2Letters[key] : 0;
+
+                if (countS1 <= 1 && countS2 <= 1) continue;
+
+                if (countS1 > countS2)
+                {
+                    results.Add($"1:{new string(key, countS1)}");
+                }
+                else if (countS2 > countS1)
+                {
+                    results.Add($"2:{new string(key, countS2)}");
+                }
+                else
+                {
+                    results.Add($"=:{new string(key, countS1)}");
+                }
+            }
+
+            results = results
+                .OrderByDescending(r => r.Length)        
+                .ThenBy(r => r[0])                        
+                .ThenBy(r => r[2])                       
+                .ToList();
+
+            return string.Join("/", results);
+        }
+
+        private static Dictionary<char, int> AddLetters(char[] letters)
+        {
+            var boxForLetters = new Dictionary<char, int>();
+
+            foreach (var letter in letters) 
+            {
+                if (char.IsLetter(letter) && char.IsLower(letter))
+                {
+                    if (boxForLetters.ContainsKey(letter))
+                    {
+                        boxForLetters[letter]++;
+                    }
+                    else
+                    {
+                        boxForLetters.Add(letter, 1);
+                    }
+                }
+            }
+
+            return boxForLetters;
+        }
+    }
+}
+//https://www.codewars.com/kata/5629db57620258aa9d000014/train/csharp
--- CodeWarsTest/UnitTest1.cs
+++ CodeWarsTest/UnitTest1.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using CodeWars4kyu;
 using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using System.Reflection;
 
 namespace CodeWarsTest
 {
@@ -11,7 +12,19 @@ namespace CodeWarsTest
 		public class KataTest
 		{
 
-			[Test]
+[Test]
+            public static void StringsMixTest()            
+            {
+                Assert.AreEqual("2:eeeee/2:yy/=:hh/=:rr", StringsMixTask.Mix("Are they here", "yes, they are here"));
+                Assert.AreEqual("2:eeeee/2:yy/=:hh/=:rr", StringsMixTask.Mix("Are they here", "yes, they are here"));
+                Assert.AreEqual("1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg", StringsMixTask.Mix("looping is fun but dangerous", "less dangerous than coding"));
+                Assert.AreEqual("1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt", StringsMixTask.Mix(" In many languages", " there's a pair of functions"));
+                Assert.AreEqual("1:ee/1:ll/1:oo", StringsMixTask.Mix("Lords of the Fallen", "gamekult"));
+                Assert.AreEqual("", StringsMixTask.Mix("codewars", "codewars"));
+                Assert.AreEqual("1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr", StringsMixTask.Mix("A generation must confront the looming ", "codewarrs"));
+            }
+
+            [Test]
 			public void ConnectFourTest()
 			{
 				var result = ConnectFourTask.WhoIsWinner(new List<string>()
@@ -28,8 +41,6 @@ namespace CodeWarsTest
 
                 Assert.AreEqual("Yellow", result);
 			}
-
-
 		}
 	}
 }
