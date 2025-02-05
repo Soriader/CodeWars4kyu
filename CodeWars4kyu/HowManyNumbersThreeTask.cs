b334ae29cc2adc875860536b1ebf29bc109d5c0e
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-05 19:48:25 +0100

    [~] Task: How many numbers III?

---
+++ CodeWars4kyu/HowManyNumbersThreeTask.cs
@@ -0,0 +1,44 @@
+﻿namespace CodeWars4kyu
+{
+    public class HowManyNumbersThreeTask
+    {
+        public static List<long> FindAll(int sumDigits, int numDigits)
+        {
+            List<long> result = new List<long>();
+            GenerateNumbers(result, new List<int>(), sumDigits, numDigits, 1);
+        
+            if (result.Count == 0)
+            {
+                return new List<long>();
+            }
+
+            return new List<long> { result.Count, result.Min(), result.Max() };
+        }
+
+        private static void GenerateNumbers(List<long> result, List<int> currentNumber, int remainingSum, int remainingDigits, int startDigit)
+        {
+            if (remainingDigits == 0)
+            {
+                if (remainingSum == 0)
+                {
+                    long number = currentNumber.Aggregate(0L, (acc, digit) => acc * 10 + digit);
+                    result.Add(number);
+                }
+                return;
+            }
+
+            for (int digit = startDigit; digit <= 9; digit++)
+            {
+                if (digit > remainingSum)
+                {
+                    break;
+                }
+
+                currentNumber.Add(digit);
+                GenerateNumbers(result, currentNumber, remainingSum - digit, remainingDigits - 1, digit);
+                currentNumber.RemoveAt(currentNumber.Count - 1);
+            }
+        }
+    }
+}
+//https://www.codewars.com/kata/5877e7d568909e5ff90017e6/train/csharp
---
+++ CodeWarsTest/HowManyNumbersThree.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeWarsTest;
+
+using CodeWars4kyu;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+public class HowManyNumbersThree
+{
+    [Test]
+    public void HowManyNumbersThreeTest()
+    {
+        CollectionAssert.AreEqual(new List<long> { 8L, 118L, 334L }, HowManyNumbersThreeTask.FindAll(10, 3));
+        CollectionAssert.AreEqual(new List<long> { 1L, 999L, 999L }, HowManyNumbersThreeTask.FindAll(27, 3));
+        CollectionAssert.AreEqual(new List<long>(), HowManyNumbersThreeTask.FindAll(84, 4));
+        CollectionAssert.AreEqual(new List<long> { 123L, 116999L, 566666L }, HowManyNumbersThreeTask.FindAll(35, 6));
+    }
+}
